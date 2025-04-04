7f8cb6536e03cb63fff408de7493125e7410c73a
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-04-04 21:39:45 +0100

    More robust compiler flags

--- tests/testthat/test-watch.R
+++ tests/testthat/test-watch.R
@@ -25,6 +25,7 @@ test_that("watcher() logs", {
 })
 
 test_that("watcher() callbacks", {
+  skip_if(R.version$arch == "aarch64" && !Sys.getenv("NOT_CRAN") == "true")
   x <- 0L
   w <- watcher(c(dir, dir2), callback = ~{is.character(.x) || stop(); x <<- x + 1L}, latency = 0.2)
   expect_output(print(w))
