4c3728488da8f061522926edd2810377957f93d1
Charlie Gao <53399081+shikokuchuo@users.noreply.github.com>
2025-07-15 18:03:12 +0100

    Pre-release commits (#26)
    * Remove test exclusions
    
    * Re-render docs

--- tests/testthat/test-watch.R
+++ tests/testthat/test-watch.R
@@ -25,7 +25,6 @@ test_that("watcher() logs", {
 })
 
 test_that("watcher() callbacks", {
-  skip_if(R.version$arch == "aarch64" && !Sys.getenv("NOT_CRAN") == "true")
   x <- 0L
   w <- watcher(
     c(dir, dir2),
